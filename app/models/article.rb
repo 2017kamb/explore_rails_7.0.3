8aeec0cabebeeec659610d51f459c46d41dc8752
Raj Kumar <raj.kumar@kgpl.com>
2022-05-24 20:22:59 +0530

    remove repeated code in the model

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,14 +5,4 @@ class Article < ApplicationRecord
   
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  VALID_STATUSES.each do |valid_status|
-    define_method "#{valid_status}?" do
-      status == valid_status
-    end
-  end
 end
